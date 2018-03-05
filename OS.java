ec7ade394c85d314eb58587fb7d00cf1633e4fa0
Hamza <hamza.parekh@gmail.com>
2018-03-04 17:28:59 -0800

    Fixed pageReplace and addPage methods.

--- OS.java
+++ OS.java
@@ -21,33 +21,6 @@ public class OS {
         p = pt;
         mem = m;
     }
-    // Default constructor for testing DELETE IT LATER!!!!!!!!!!!!!
-    public OS() throws IOException {
-    }
-
-    public static void main(String[] args) throws IOException {
-        // to test that Copying is happening correctly
-        //OS op = new OS();
-        //op.addPage("AA00");
-
-        // PageTable p = new PageTable();
-        // TLBcache tl = new TLBcache();
-        // System.out.println(tl.getEntry(0).getReference());
-        // tl.getEntry(0).setReference();
-        // System.out.println(tl.getEntry(0).getReference());
-        // op.resetRef(p, tl);
-        // System.out.println(tl.getEntry(0).getReference());
-        // System.out.println(op);
-
-        // if(pfolder.isDirectory())
-        //     System.out.println("Got IT!!!");
-        // else
-        //     System.out.println("Don't Got it (-_-)");
-
-        // for(File pfile: pfolder.listFiles()){
-        //     System.out.println(pfile.getName());
-        // }
-    }
 
     // reset reference bits must pass in TLB
     public void resetRef(TLBcache t) {
@@ -88,7 +61,7 @@ public class OS {
             dirtySet = replace[2];
         }
         else{
-            p.getEntry(frameNo).writeEntry(writeMemory(pFile)); // empty index written
+            p.getEntry(frameNo).writeEntry(mem.writeMemory(pFile)); // empty index written
         }
         
         int[] ret = {evictedPage, dirtySet};
@@ -101,22 +74,6 @@ public class OS {
         int evictedPage = -1;
         int dirtySet = -1;
         StringBuilder sb = new StringBuilder(); // new stringbuilder to store output
-        
-        // // reset R bit until an entry is found with R bit already == 0
-        // while(p.getEntry(pointer).getReference() == 1){
-        //     p.getEntry(pointer).resetReference();
-        //     // If page table entry is also in TLB reset its R bit
-        //     if(t.getEntry(pointer) != null){
-        //         t.getEntry(pointer).resetReference();
-        //     }
-        //     // go to next page table entry
-        //     if(pointer == p.PT.length-1){
-        //         pointer = 0;
-        //     }
-        //     else{
-        //         pointer++;
-        //     }
-        // }
         
         boolean cycle = true;
         while(cycle == true){
@@ -140,12 +97,13 @@ public class OS {
             }
         }
 
-
-
-
         // if the page table at pointer already has (R bit == 0) and (V bit == 1)
         if(p.getEntry(pointer).getDirty() == 1){
-            pw = new PrintWriter(new File("EditedPageFiles/" + Integer.toHexString(pointer) + ".pg")); // to overwrite the page file
+            String tmp = Integer.toHexString(pointer);
+            if(tmp.length() == 1){
+                tmp = "0" + tmp;
+            }
+            pw = new PrintWriter(new File("EditedPageFiles/" + tmp + ".pg")); // to overwrite the page file
             int RAMindex = p.getEntry(pointer).getPageframe();
             for(int i=0; i<mem.RAM[RAMindex].length;i++){
                 sb.append(mem.RAM[RAMindex][i] + "\n");
@@ -162,7 +120,7 @@ public class OS {
         writeIndex = p.getEntry(pointer).getPageframe(); // the open index in RAM
         p.PT[pointer] = new PageTableEntry(); // resets bits in Entry
         
-        // SHOULD THERE BE A POINTER++ HERE SO IT WILL START ON THE NEXT ONE NEXT TIME?????????????????????????????????
+        pointer++; // iterate pointer
 
         int[] ret = {writeIndex, evictedPage, dirtySet};
         return ret;
